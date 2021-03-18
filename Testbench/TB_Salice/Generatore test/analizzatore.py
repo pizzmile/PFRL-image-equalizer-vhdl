483d489031827012680e8444b653bffa0ffe6a95
P1224 <giacomo.pizza99@gmail.com>
2021-03-18 01:40:56 +0100

    analyzer

--- Testbench/TB_Salice/Generatore test/analizzatore.py
+++ Testbench/TB_Salice/Generatore test/analizzatore.py
@@ -10,105 +10,96 @@ def findAll(source, value):
     return indexes
 
 
-def analyzeTest(filename):
-    with open(filename, 'r') as file:
+def analyzeTest():
+    with open('test1.txt') as file:
         src = file.readlines()
+    src = [s[:-1] for s in src if s != '\n']
 
-    idx = findAll(src, 'TEST:')
+    test_indexes = findAll(src, 'TEST')
+    test_json = {
+        'count': len(test_indexes),
+        'passed': len(test_indexes),
+        'failed': 0,
+        'tests': []
+    }
+    for i in range(len(test_indexes)):
+        idx = test_indexes[i]
 
-    substr = []
-    for i in range(len(idx)):
-        substr.append(src[idx[i - 1]:idx[i]])
-    substr = substr[1:]
+        id = int(src[idx].split(' ')[-1])
+        rows = int(src[idx+1].split(' ')[1])
+        columns = int(src[idx+1].split(' ')[-1])
 
-    jsons = {'count': 0,
-             'passed': 0,
-             'failed': 0,
-             'failed_tests': [],
-             'tests': []}
-    for s in substr:
-        dimensions = [int(x) for x in s[1][:-1].replace('Rows: ', '').replace('Columns: ', '').split('    ')]
-        image = []
-        image_origin = s[s.index('Image\n') + 1: s.index('Solution\n') - 1]
-        for pixels in image_origin:
-            pixels = [int(pixel) for pixel in pixels.split() if pixel.isdigit()]
-            image += pixels
+        if rows != 0 and columns != 0:
+            image_idx = idx + 2
+            if i < len(test_indexes)-1:
+                solution_idx = src[idx:test_indexes[i+1]].index('Solution') + idx
+                solution = [[int(s) for s in i.split() if s.isdigit()] for i in src[solution_idx + 1:test_indexes[i+1]]]
+            else:
+                solution_idx = src[idx:].index('Solution') + idx
+                solution = [[int(s) for s in i.split() if s.isdigit()] for i in src[solution_idx + 1:]]
+            image = [[int(s) for s in i.split() if s.isdigit()] for i in src[image_idx+1:solution_idx]]
+            image = [item for sublist in image for item in sublist]
+            solution = [item for sublist in solution for item in sublist]
 
-        solution = []
-        solution_origin = s[s.index('Solution\n')+1:]
-        for pixels in solution_origin:
-            pixels = [int(pixel) for pixel in pixels.split() if pixel.isdigit()]
-            solution += pixels
+            min_value = min(image)
+            max_value = max(image)
+            delta_value = max_value - min_value
+            shift_level = 8 - floor(log2(delta_value + 1))
+            shift_multiplier = pow(2, shift_level)
+        else:
+            image = []
+            solution = []
+            min_value = None
+            max_value = None
+            delta_value = None
+            shift_level = None
+            shift_multiplier = None
 
-        min_value = 255
-        max_value = 0
-        for pixel in image:
-            if pixel > max_value:
-                max_value = pixel
-            if pixel < min_value:
-                min_value = pixel
-        delta_value = abs(max_value - min_value)
-        shift_level = 8 - floor(log2(delta_value + 1))
-
-        test_json = {
-            'title': s[0][:-1],
+        test_json['tests'].append({
+            'id': id,
             'passed': True,
             'dimensions': {
-                'rows': dimensions[0],
-                'columns': dimensions[1]
+                'rows': rows,
+                'columns': columns
+            },
+            'aggregate parameters': {
+                'min value': min_value,
+                'max value': max_value,
+                'delta value': delta_value,
+                'shift level': shift_level,
+                'shift multiplier': shift_multiplier
             },
             # 'image': image,
             # 'solution': solution,
-            'values': {
-                'min_value': min_value,
-                'max_value': max_value,
-                'delta_value': delta_value,
-                'shift_level': shift_level
-            }
-        }
-        jsons['tests'].append(test_json)
+        })
 
-    jsons['count'] = len(jsons['tests']) + 1
-    jsons['passed'] = jsons['count']
+    with open('ramNotPassed.txt') as file:
+        src = file.readlines()
+    src = [s[:-1].replace('TEST ', '').split(':') for s in src if s != '\n']
 
-    with open(test_json_filename, 'w') as outfile:
-        json.dump(jsons, outfile, indent=2)
+    last_id = 0
+    failed_count = 0
+    for s in src:
+        if int(s[0]) != last_id:
+            last_id = int(s[0])
+            for test in test_json['tests']:
+                if test['id'] == last_id:
+                    test['passed'] = False
+            failed_count += 1
+    print(failed_count)
+    test_json['passed'] -= failed_count
+    test_json['failed'] += failed_count
+    print(test_json['count'])
+
+    with open('analysis.json', 'w') as outfile:
+        json.dump(test_json, outfile, indent=2)
+
+
+
 
 
 def analyzeNotPassed(filename):
-    with open(test_json_filename) as file:
-        test_json = json.load(file)
+    pass
 
-    with open(filename, 'r') as file:
-        src = file.readlines()
-    src = [s[:-1].split(": ") for s in src]
-    for sub in src:
-        sub[0] = int(sub[0].replace('TEST ', ''))
-        sub[1] = [int(s) for s in sub[1].replace('pixel ', '').replace('expected ', '').replace('found ', '').split(' ')]
-        sub += sub[1]
-        del sub[1]
 
-    last = 0
-    failed_count = 0
-    for sub in src:
-        if sub[0] != last:
-            failed_count += 1
-            last = sub[0]
-            test_json['failed_tests'].append(sub[0])
-            test_json['tests'][last-1]['passed'] = False
-
-    test_json['passed'] -= failed_count
-    test_json['failed'] = failed_count
-
-    with open(test_json_filename, 'w') as outfile:
-        json.dump(test_json, outfile, indent=2)
-
-src = None
-
-test_filename = 'test1.txt'
-test_json_filename = f'analysis_{test_filename[:test_filename.index(".")]}.json'
-# input('Enter a filename: ')
-notPassed_filename = 'ramNotPassed.txt'
-
-analyzeTest(test_filename)
-analyzeNotPassed(notPassed_filename)
+analyzeTest()
